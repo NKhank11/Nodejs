16588ef243623c5e207595959bbfb2b1a15e7d8d
Nguyen Khanh <namkhanhto23@gmail.com>
2024-08-16 21:01:14 +0700

    Danh muc san pham

---
+++ controllers/admin/product-category.controller.js
@@ -0,0 +1,37 @@
+const ProductCategory =require("../../models/product-category.model");
+const systemConfig = require("../../config/system");
+
+// [GET] /admin/product-category
+module.exports.index = async (req, res) => {
+  let find = {
+    deleted: false
+  }
+  const records = await ProductCategory.find(find);
+
+  res.render("admin/pages/product-category/index", {
+    pageTitle: "Danh mục sản phẩm",
+    records: records,
+  });
+}
+
+// [GET] /admin/product-category
+module.exports.create = async (req, res) => {
+  res.render("admin/pages/product-category/create", {
+    pageTitle: "Tạo danh mục sản phẩm",
+  });
+}
+
+// [POST] /admin/product-category/create
+module.exports.createPost = async (req, res) => {
+  if(req.body.position == "") {
+    const count = await ProductCategory.countDocuments();
+    req.body.position = count + 1;
+  }
+  else {
+    req.body.position = parseInt(req.body.position);
+  }
+  const record = new ProductCategory(req.body);  // req.body là dữ liệu từ form
+  await record.save();   // Lưu vào database
+  req.flash("success", "Thêm danh mục thành công !");
+  res.redirect(`${systemConfig.prefixAdmin}/product-category`);
+}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
 const methodOverride = require('method-override');
 const bodyParser = require("body-parser");
@@ -25,6 +26,12 @@ app.use(cookieParser('HDSBGKSKJS')); // Key tự tạo để mã hóa cookie
 app.use(session({ cookie: { maxAge: 60000 }}));
 app.use(flash());
 // End Flash
+
+// TinyMCE
+app.use(
+  '/tinymce',
+  express.static(path.join(__dirname, 'node_modules', 'tinymce')));
+// End TinyMCE
 
 
 // parse application/x-www-form-urlencoded
