4b4c39074d9e5e0c34728fa884e0900287c25fc9
Nguyen Khanh <namkhanhto23@gmail.com>
2024-08-20 22:13:50 +0700

    Làm trang sửa tài khoản

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -11,7 +11,7 @@ module.exports.index = async (req, res) => {
   };
   const records = await Account.find(find).select("-password -token");
 
-  for(const record of records) {
+  for (const record of records) {
     const role = await Role.findOne({
       _id: record.role_id,
       deleted: false,
@@ -55,4 +55,60 @@ module.exports.createPost = async (req, res) => {
     req.flash("success", "Tạo tài khoản thành công");
     res.redirect(`${systemConfig.prefixAdmin}/accounts`);
   }
+}
+
+// [GET] /admin/accounts/edit/:id
+module.exports.edit = async (req, res) => {
+  try { 
+    let find = {
+      _id: req.params.id,
+      deleted: false
+    };
+    const record = await Account.findOne(find);
+
+    const roles = await Role.find({
+      deleted: false
+    })
+
+    console.log(record);
+
+    res.render("admin/pages/accounts/edit", {
+      pageTitle: "Chỉnh sửa tài khoản",
+      record: record,
+      roles: roles,
+    });
+  } catch (error) {
+    req.flash("error", "Không tìm thấy tài khoản");
+    res.redirect(`${systemConfig.prefixAdmin}/accounts`);
+  }
+}
+
+// [PATCH] /admin/accounts/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+
+  const emailExist = await Account.findOne({
+    _id: { $ne: id },
+    email: req.body.email,
+    deleted: false
+  })
+  
+  if(emailExist) {
+    req.flash("error", "Email đã tồn tại");
+  }
+  else {
+    if(req.body.password) {
+      req.body.password = md5(req.body.password);
+    }
+    else {
+      delete req.body.password;
+    }
+  
+    await Account.updateOne({
+      _id: id
+    }, req.body);
+  
+    req.flash("success", "Chỉnh sửa tài khoản thành công");
+  }
+  res.redirect(`back`);
 }
--- validate/admin/account.validate.js
+++ validate/admin/account.validate.js
@@ -13,3 +13,16 @@ module.exports.createPost = (req, res, next) => {
   }
   next();
 }
+
+module.exports.editPatch = (req, res, next) => {
+  if(!req.body.fullName) {
+    req.flash("error", "Vui lòng nhập họ tên!");
+    return res.redirect("back");
+  }
+  if(!req.body.email) {
+    req.flash("error", "Vui lòng nhập email!");
+    return res.redirect("back");
+  }
+
+  next();
+}
