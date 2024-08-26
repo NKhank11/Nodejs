9d994b990e2265f85b15a4b55eff8706e6c3585d
Nguyen Khanh <namkhanhto23@gmail.com>
2024-08-26 19:01:45 +0700

    Trang chi tiết sản phẩm bên client

--- helpers/product.js
+++ helpers/product.js
@@ -5,3 +5,8 @@ module.exports.priceNewProducts = (productsFeatured) => {
   });
   return newProducts;
 }
+
+module.exports.priceNewProduct = (product) => {
+  const priceNew = (product.price - product.price * product.discountPercentage / 100).toFixed(0);
+  return priceNew;
+}
