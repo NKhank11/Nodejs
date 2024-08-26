53ff8898faa43a0f46241d2f820b42b9ab8784a9
Nguyen Khanh <namkhanhto23@gmail.com>
2024-08-26 14:34:52 +0700

    Hiển thị danh sách sản phẩm mới bên client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -13,8 +13,18 @@ module.exports.index = async (req, res) => {
 
   const newProducts = productHelper.priceNewProducts(productsFeatured);
 
+  // Hiển thị danh sách sản phẩm mới nhất
+  const productsNew = await Product.find({
+    deleted: false,
+    status: "active",
+  }).sort({position: "desc"} ).limit(6)
+  // Hết Hiển thị danh sách sản phẩm mới nhất
+
+  const newProductsNew = productHelper.priceNewProducts(productsNew);
+
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
-    productsFeatured: newProducts
+    productsFeatured: newProducts,
+    productsNew: newProductsNew,
   });
 }
