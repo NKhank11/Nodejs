a713ab5ac55068c30fd01b3c7455a461b69f4f94
Nguyen Khanh <namkhanhto23@gmail.com>
2024-08-26 14:23:30 +0700

    Hiển thị danh sách sản phẩm nổi bật

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,20 @@
+const Product = require("../../models/product.model");
+const productHelper = require("../../helpers/product.js");
+
 // [GET] /
 module.exports.index = async (req, res) => {
+  // Lấy ra sản phẩm nổi bật
+  const productsFeatured = await Product.find({
+    featured: "1",
+    deleted: false,
+    status: "active",
+  })
+  // Hết lấy ra sản phẩm nổi bật
+
+  const newProducts = productHelper.priceNewProducts(productsFeatured);
+
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
+    productsFeatured: newProducts
   });
 }
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../../models/product.model")
+const productHelper = require("../../helpers/product.js");
 
 // [GET] /products
 module.exports.index = async (req, res) => {
@@ -13,10 +14,7 @@ module.exports.index = async (req, res) => {
   // })
 
   // Mảng mới
-  const newProducts = products.map((item) => {
-    item.priceNew = (item.price - item.price * item.discountPercentage / 100).toFixed(0);
-    return item;
-  })
+  const newProducts = productHelper.priceNewProducts(products);
   res.render("client/pages/products/index", {
     pageTitle: "Trang Sản phẩm",
     products: newProducts
