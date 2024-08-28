ce10cb4ad3830dc91b362d31452e1377cd5db513
Nguyen Khanh <namkhanhto23@gmail.com>
2024-08-28 20:41:01 +0700

    Làm trang giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,4 +1,38 @@
 const Cart = require("../../models/cart.model");
+const Product = require("../../models/product.model");
+
+const productsHelper = require("../../helpers/product");
+
+// [GET] /cart/
+module.exports.index = async (req, res) => {
+  const cartId = req.cookies.cartId;
+
+  const cart = await Cart.findOne({
+    _id: cartId
+  });
+
+  if(cart.products.length > 0) {
+    for(const item of cart.products) {
+      const productId = item.product_id;
+      const productInfo = await Product.findOne({
+        _id: productId,
+      }).select("title thumbnail slug price discountPercentage"); 
+
+      productInfo.priceNew = parseInt(productsHelper.priceNewProduct(productInfo));
+
+      item.productInfo = productInfo;
+
+      item.totalPrice = productInfo.priceNew * item.quantity;
+    }
+  } 
+
+  cart.totalPrice = cart.products.reduce((total, item) => total + item.totalPrice, 0);
+
+  res.render("client/pages/cart/index", {
+    pageTitle: "Giỏ hàng",
+    cartDetail: cart
+  })
+}
 
 // [POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
@@ -46,3 +80,4 @@ module.exports.addPost = async (req, res) => {
   req.flash("success", "Đã thêm sản phẩm vào giỏ hàng");
   res.redirect("back");
 }
+
