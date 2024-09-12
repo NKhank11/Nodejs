89a3319161e275441ab4c3f28fba39f55a97ee1e
Nguyen Khanh <namkhanhto23@gmail.com>
2024-09-12 15:13:15 +0700

    Tối ưu source code

--- index.js
+++ index.js
@@ -26,10 +26,7 @@ const port = process.env.PORT;
 // SocketIO 
 const server = http.createServer(app);
 const io = new Server(server); 
-
-io.on("connection", (socket) => {
-  console.log('A user connected', socket.id);
-})
+global._io = io;
 
 // End SocketIO
 
