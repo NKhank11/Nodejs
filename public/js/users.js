9bc05def3d53ba5035540d3d195a720f04545205
Nguyen Khanh <namkhanhto23@gmail.com>
2024-09-19 15:44:04 +0700

    Chức năng hủy yêu cầu kết bạn

--- public/js/users.js
+++ public/js/users.js
@@ -12,3 +12,18 @@ if(listBtnAddFriend) {
   })
 }
 // Hết Chức năng gửi yêu cầu
+
+// Chức năng hủy gửi yêu cầu
+const listBtnCancelFriend = document.querySelectorAll("[btn-cancel-friend]");
+if(listBtnCancelFriend) {
+  listBtnCancelFriend.forEach(button => {
+    button.addEventListener("click", () => {
+      button.closest(".box-user").classList.remove("add");
+
+      const userId = button.getAttribute("btn-cancel-friend");
+      
+      socket.emit("CLIENT_CANCEL_FRIEND", userId);
+    })
+  })
+}
+// Hết Chức năng hủy gửi yêu cầu
--- socket/client/users.socket.js
+++ socket/client/users.socket.js
@@ -2,6 +2,7 @@ const User = require("../../models/user.model");
 
 module.exports = (res) => {
   _io.once("connection", (socket) => {
+    // Chức năng gửi yêu cầu
     socket.on("CLIENT_ADD_FRIEND", async (userId) => {
       const myUserId = res.locals.user.id; 
 
@@ -37,5 +38,44 @@ module.exports = (res) => {
       }
 
     });
+    // Hết Chức năng gửi yêu cầu
+
+
+    // Chức năng hủy gửi yêu cầu
+    socket.on("CLIENT_CANCEL_FRIEND", async (userId) => {
+      const myUserId = res.locals.user.id; 
+
+      // console.log(myUserId); // Id của A
+      // console.log(userId); // Id của B
+
+      // Xóa Id của A trong acceptFriends của B
+      const existsIdAinB = await User.findOne({
+        _id: userId,
+        acceptFriends: myUserId
+      });
+
+      if(existsIdAinB) {
+        await User.updateOne({
+          _id: userId
+        }, {
+          $pull: { acceptFriends: myUserId}
+        })
+      }
+
+      // Xóa Id của B trong requestFriends của A  
+      const existsIdBinA = await User.findOne({
+        _id: myUserId,
+        requestFriends: userId
+      });
+
+      if(existsIdBinA) {
+        await User.updateOne({
+          _id: myUserId
+        }, {
+          $pull: { requestFriends: userId}
+        });
+      }
+    });
+    // Hết Chức năng hủy gửi yêu cầu
   });
 }
