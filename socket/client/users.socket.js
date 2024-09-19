cbfa35e7b89d70e06557b79976fe6586e116235b
Nguyen Khanh <namkhanhto23@gmail.com>
2024-09-19 21:15:13 +0700

    Khi A hủy lời mời, cập nhật số lượng mời bên B

--- socket/client/users.socket.js
+++ socket/client/users.socket.js
@@ -87,6 +87,19 @@ module.exports = (res) => {
           $pull: { requestFriends: userId}
         });
       }
+
+      // Lấy ra độ dài acceptFriends của B và trả về cho B
+      const infoUser = await User.findOne({
+        _id: userId
+      });
+
+      const lengthAcceptFriends = infoUser.acceptFriends.length;
+
+      socket.broadcast.emit("SERVER_RETURN_LENGTH_ACCEPT_FRIEND", {
+        userId: userId,
+        lengthAcceptFriends: lengthAcceptFriends
+      });
+
     });
     // Hết Chức năng hủy gửi yêu cầu
 
