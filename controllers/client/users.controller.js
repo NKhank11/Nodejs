e61ea2cd9ea7c71db980a7a3756b4565bdf3c56c
Nguyen Khanh <namkhanhto23@gmail.com>
2024-09-20 21:47:01 +0700

    Hiển thị tag online

--- controllers/client/users.controller.js
+++ controllers/client/users.controller.js
@@ -84,3 +84,30 @@ module.exports.accept = async (req, res) => {
     users: users
   })
 }
+
+// [GET] /users/friends
+module.exports.friends = async (req, res) => {
+  // Socket
+  usersSocket(res);
+  // End Socket
+
+  const userId = res.locals.user.id;
+
+  const myUserId = await User.findOne({
+    _id: userId
+  });
+
+  const friendList = myUserId.friendList;
+  const friendListId = friendList.map(friend => friend.user_id);
+
+  const users = await User.find({
+    _id: { $in: friendListId },
+    status: "active",
+    deleted: false,
+  }).select("id avatar fullName statusOnline");
+
+  res.render("client/pages/users/friends", {
+    pageTitle: "Danh sách bạn bè",
+    users: users
+  })
+}
