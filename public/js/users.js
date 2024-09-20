0e21bedb7b93de4ac12d67a1e34955b43edbc330
Nguyen Khanh <namkhanhto23@gmail.com>
2024-09-20 20:31:47 +0700

    A hủy kết bạn sẽ xóa A khỏi danh sách lời mời của B (đang lỗi)

--- public/js/users.js
+++ public/js/users.js
@@ -91,6 +91,7 @@ if (dataUsersAccept) {
       // Vẽ user ra giao diện
       const div = document.createElement("div");
       div.classList.add("col-6");
+      div.setAttribute("user-id", data.infoUserA._id);
 
       div.innerHTML = `
         <div class="box-user">
@@ -144,3 +145,16 @@ if (dataUsersAccept) {
   });
 }
 // Hết SERVER_RETURN_INFO_ACCEPT_FRIEND
+
+// SERVER_RETURN_USER_ID_CANCEL_FRIEND
+socket.on("SERVER_RETURN_USER_ID_CANCEL_FRIEND", (data) => {
+  const boxUserRemove = document.querySelector(`[user-id='${data.userIdA}']`);
+  if(boxUserRemove) {
+    const dataUsersAccept = document.querySelector("[data-users-accept]");
+    const userIdB = badgeUsersAccept.getAttribute("badge-users-accept");
+    if(userIdB === data.userIdB) {
+      dataUsersAccept.removeChild(boxUserRemove);
+    }
+  }
+})
+// Hết SERVER_RETURN_USER_ID_CANCEL_FRIEND
